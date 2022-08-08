4157f585344e84e647797f6ac2f4eb8597e1d60c
kohj1018 <kohj1018@gmail.com>
2022-08-08 18:11:47 +0900

    [fix][PILLUP-330] 영양제 삭제 시 버그 수정

--- components/common/PillManagementListItem.tsx
+++ components/common/PillManagementListItem.tsx
@@ -13,9 +13,9 @@ interface Props {
 
 function PillManagementListItem({ maker, name, id, deleteFunc }: Props) {
   return (
-    <Link href={`/pill-details/${id}`}>
-      <a>
-        <div className='flex items-center justify-between w-full h-28 border-[#BABABA] border rounded-3xl px-2 space-x-2'>
+    <div className='flex items-center justify-between w-full h-28 border-[#BABABA] border rounded-3xl px-2'>
+      <Link href={`/pill-details/${id}`}>
+        <a className='flex items-center w-4/5 h-full space-x-5'>
           <div className='relative w-24 h-24 rounded-3xl border-[#BABABA] border overflow-hidden'>
             <Image
               src={
@@ -30,16 +30,16 @@ function PillManagementListItem({ maker, name, id, deleteFunc }: Props) {
             <p className='text-lg text-[#7A7A7A]'>{maker}</p>
             <p className='text-lg'>{name}</p>
           </div>
-          <div className='border-l border-[#BABABA] w-1/5 h-full flex items-center justify-center'>
-            <FontAwesomeIcon
-              icon={faTrashCan}
-              className='text-2xl text-red-600 z-10'
-              onClick={() => deleteFunc(id)}
-            />
-          </div>
-        </div>
-      </a>
-    </Link>
+        </a>
+      </Link>
+      <div className='border-l border-[#BABABA] w-1/5 h-full flex items-center justify-center'>
+        <FontAwesomeIcon
+          icon={faTrashCan}
+          className='text-2xl text-red-600 z-10'
+          onClick={() => deleteFunc(id)}
+        />
+      </div>
+    </div>
   )
 }
 
