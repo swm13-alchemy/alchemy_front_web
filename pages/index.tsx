0aae825278f2289dff154329930a83539bfe2b2c
kohj1018 <kohj1018@gmail.com>
2022-09-06 21:47:48 +0900

    [add][PILLUP-455] 홈화면 퍼블리싱(임시)

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,11 +5,14 @@ import DateBox from '../components/common/DateBox'
 import MyPillList from '../components/common/MyPillList'
 import ContainerWithBottomNav from '../components/layout/ContainerWithBottomNav'
 import MainHeader from '../components/layout/MainHeader'
+import Image from 'next/image'
+import bigLogo from '../public/asset/image/bigLogo.png'
+import React from 'react'
 
 const Home: NextPage = () => {
   return (
     <ContainerWithBottomNav>
-      <SEO title='PillUp: 스마트하게 건강 지키기' />
+      <SEO title='BeeHealer' />
 
       {/*<div className='flex flex-col space-y-4'>*/}
       {/*  <header className='flex flex-col space-y-4 bg-white pb-4'>*/}
@@ -33,9 +36,19 @@ const Home: NextPage = () => {
       {/*  </div>*/}
       {/*</div>*/}
 
-      <div className='bg-white h-screen flex flex-col space-y-32'>
-        <MainHeader />
+      <div className='bg-white h-screen flex flex-col'>
+        <div className='relative w-[15.625rem] h-[3.83254rem] mx-auto mt-32 mb-10'>
+          <Image
+            src={bigLogo}
+            className='object-cover'
+            layout='fill'
+          />
+        </div>
         <SearchBtn />
+
+        <div className='mt-28'>
+          <MyPillList />
+        </div>
       </div>
 
 
