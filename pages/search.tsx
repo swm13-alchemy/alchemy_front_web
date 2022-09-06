4dad19f5cc60aa18cfdbe1edf8c8a00f4b6dfece
kohj1018 <kohj1018@gmail.com>
2022-09-07 02:53:19 +0900

    [add] 검색 페이지 검색 결과 없으면 없다고 표시 처리

--- pages/search.tsx
+++ pages/search.tsx
@@ -8,18 +8,26 @@ import BackHeader from '../components/layout/BackHeader'
 import { NextPage } from 'next'
 import ContainerWithBottomNav from '../components/layout/ContainerWithBottomNav'
 import { arrayIsNotEmpty } from '../utils/functions/arrayIsNotEmpty'
+import Image from 'next/image'
+import noSearchResult from '../public/asset/image/noSearchResult.png'
 
 const Search: NextPage = () => {
   const router = useRouter()
   // const [formInputs, setFormInputs] = useState({})
   const [searchTerm, setSearchTerm] = useState<string>('')
   const [searchResults, setSearchResults] = useState<SearchResultsItemType[]>([])
+  const [isNoResult, setIsNoResult] = useState<boolean>(false)
 
   useEffect(() => {
     if (router.query.name) {
-      (async function reloadingSearch() {
+      (async function reloadingSearch() { // 쿼리 스트링 보고 검색하는 함수
         const { data: { data: result } } = await pillApi.getSearchResults(router.query.name)
-        setSearchResults(result)
+        if (arrayIsNotEmpty(result)) {
+          setIsNoResult(false)
+          setSearchResults(result)
+        } else {  // 받아온 data의 배열이 비어있는 경우 isNoResult를 true로 바꿈
+          setIsNoResult(true)
+        }
       })()
     }
   }, [router.query.name])
@@ -46,21 +54,31 @@ const Search: NextPage = () => {
         searchTerm={searchTerm}
       />
 
-      {arrayIsNotEmpty(searchResults) && (
-        <div className='px-6 pt-2'>
-          <p className='text-gray-900 text-base'>검색된 영양제 {searchResults?.length}개</p>
-          <div className='flex flex-col w-full mt-4 space-y-4'>
-            {searchResults.map((supplement) => {
-              return (
-                <SearchResultListItem
-                  key={supplement.name}
-                  id={supplement.id}
-                  name={supplement.name}
-                  maker={supplement.maker}
-                />
-              )
-            })}
+      {!isNoResult ? (  // 검색 결과가 있는 경우
+        arrayIsNotEmpty(searchResults) && (
+          <div className='px-6 pt-2'>
+            <p className='text-gray-900 text-base'>검색된 영양제 {searchResults?.length}개</p>
+            <div className='flex flex-col w-full mt-4 space-y-4'>
+              {searchResults.map((supplement) => {
+                return (
+                  <SearchResultListItem
+                    key={supplement.name}
+                    id={supplement.id}
+                    name={supplement.name}
+                    maker={supplement.maker}
+                  />
+                )
+              })}
+            </div>
           </div>
+        )
+      ) : ( // 검색 결과가 없는 경우
+        <div className='!mt-60 mx-auto relative w-[10.3125rem] h-[4.625rem]'>
+          <Image
+            src={noSearchResult}
+            className='object-cover'
+            layout='fill'
+          />
         </div>
       )}
     </div>
