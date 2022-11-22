d6c75b8703c479073ef7faf40bc864c335c0c9cd
kohj1018 <kohj1018@gmail.com>
2022-11-22 19:21:43 +0900

    카메라 권한 해결

--- utils/functions/isMobile.ts
+++ utils/functions/isMobile.ts
@@ -1,11 +1,3 @@
-import { useEffect, useState } from 'react'
-
 export function isMobile(): boolean {
-  const [isMobileBoolean, setIsMobileBoolean] = useState<boolean>(false)
-
-  useEffect(() => {
-    setIsMobileBoolean(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent))
-  }, [])
-
-  return isMobileBoolean
+  return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)
 }
